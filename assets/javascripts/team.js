88e2223b2511b4cd6122786147c9d650861ec9f0
Matthew Basset <mbasset@gmail.com>
2013-02-10 12:41:42 -0500

    Add back in missing colours from team members.

--- assets/javascripts/team.js
+++ assets/javascripts/team.js
@@ -2,12 +2,17 @@
  * General algorithmn for hex colurs adapted from: http://paulirish.com/2009/random-hex-color-code-snippets/
  */
 
-$(function() {
-	$(".po_team_member").each(function( index, element ) {
+var po_team_colourize = function(elements) {
+	elements = typeof elements !== 'undefined' ? elements : $(".po_team_member");
+	elements.each(function( index, element ) {
 		var $this = $(element);
 		var colours =	["#F7977A", "#FDC68A", "#FFF79A", "#C4DF9B", "#7BCDC8", "#7EA7D8", "#BC8DBF", "#F6989D"];
 		
 		var bg = parseInt($this.find(".name").data("id")) % colours.length;
 		$this.css("background-color", colours[bg]);
 	});
+};
+
+$(function() {
+	po_team_colourize;
 });
