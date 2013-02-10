97a0bdcaf431b7ef916885c8e06dc340ec71725f
Matthew Basset <mbasset@gmail.com>
2013-02-10 13:03:14 -0500

    Lighten up the team member colours.

--- assets/javascripts/team.js
+++ assets/javascripts/team.js
@@ -6,7 +6,7 @@ var po_team_colourize = function(elements) {
 	elements = typeof elements !== 'undefined' ? elements : $(".po_team_member");
 	elements.each(function( index, element ) {
 		var $this = $(element);
-		var colours =	["#F7977A", "#FDC68A", "#FFF79A", "#C4DF9B", "#7BCDC8", "#7EA7D8", "#BC8DBF", "#F6989D"];
+		var colours =	["#FFF1ED", "#FDECDA", "#FFFCD9", "#EEFFD4", "#EDFFFE", "#E0EEFF", "#F4E6F5", "#FFE8E9"];
 		
 		var bg = parseInt($this.find(".name").data("id")) % colours.length;
 		$this.css("background-color", colours[bg]);
