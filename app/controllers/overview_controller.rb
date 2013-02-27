e389b4c7a60cb21ead7413710ae930f50be3db26
Matthew Basset <matthew.basset@softgravity.com>
2013-02-27 08:41:04 -0500

    Add in the ability to sort projects by project activity over given number of days.

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -1,6 +1,6 @@
 class OverviewController < ApplicationController
   unloadable
-  
+
   helper :project_overview
   helper :custom_fields
 
@@ -26,7 +26,17 @@ class OverviewController < ApplicationController
     if Setting.plugin_project_overview.has_key?(:exclude_projects)
       scope = scope.where("id NOT IN (?)", Setting.plugin_project_overview[:exclude_projects])
     end
-    @projects = scope.visible.order('name ASC').all
+
+    scope = scope.visible
+
+    @projects = case Setting.plugin_project_overview[:project_order]
+    when "by_activity"
+      scope.sort_by do |p|
+        -1*Redmine::Activity::Fetcher.new(User.current, :project => p).events(Date.today - Setting.plugin_project_overview[:activity_sum_days].to_i.days, Date.today).count
+      end
+    else
+      scope.order('name ASC')
+    end
   end
 
   def team_index
