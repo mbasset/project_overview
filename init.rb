3692564e7a5ef1b2892c176b82052a7046547d82
Matthew Basset <matthew.basset@softgravity.com>
2013-08-26 12:06:23 -0400

    Version bump to 1.2.0

--- init.rb
+++ init.rb
@@ -1,24 +1,24 @@
 require 'redmine'
 
 Redmine::Plugin.register :project_overview do
-  name 'Project Overview'
-  author 'Matthew Basset'
-  description 'Updates the project overview page to make it more useful for teams.'
-  version '1.1.0'
-  url 'https://github.com/mbasset/project_overview'
-  author_url 'http://mbasset.info'
+	name 'Project Overview'
+	author 'Matthew Basset'
+	description 'Updates the project overview page to make it more useful for teams.'
+	version '1.2.0'
+	url 'https://github.com/mbasset/project_overview'
+	author_url 'http://mbasset.info'
 
-  menu :top_menu, :overview, { :controller => 'overview', :action => 'index' }, :before => :projects
+	menu :top_menu, :overview, { :controller => 'overview', :action => 'index' }, :before => :projects
 
-  settings :default => {
-    :stale_enable => true,
-    :stale_num_days => 14,
-    :exclude_projects => [],
-    :inactive_team_num_days => 14,
-    :project_order => "by_name",
-    :activity_sum_days => 14,
-    :velocity_warning_margin => 5
-  }, :partial => 'overview/settings'
+	settings :default => {
+		:stale_enable => true,
+		:stale_num_days => 14,
+		:exclude_projects => [],
+		:inactive_team_num_days => 14,
+		:project_order => "by_name",
+		:activity_sum_days => 14,
+		:velocity_warning_margin => 5
+	}, :partial => 'overview/settings'
 end
 
 require 'settings_helper'
